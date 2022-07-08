3b19a0974846805bd0427504821ad9ccf923715e
HenryFarias <henrysjfarias@gmail.com>
2022-07-07 22:00:17 -0300

    Create tests to planetDto

---
+++ src/test/java/br/com/elo7/sonda/candidato/planet/dto/PlanetDtoTest.java
@@ -0,0 +1,91 @@
+package br.com.elo7.sonda.candidato.planet.dto;
+
+import org.jeasy.random.EasyRandom;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class PlanetDtoTest {
+
+    private final EasyRandom generator = new EasyRandom();
+
+    @Test
+    public void should_verify_coordinate_inside_planet_with_x_bigger_then_planet_error() {
+        var planet = generator.nextObject(PlanetDTO.class);
+        planet.setWidth(5);
+        planet.setHeight(5);
+        Exception appException = assertThrows(Exception.class, () ->
+            planet.coordinatesIsInsidePlanet(7, 0)
+        );
+        assertEquals("Object is off planet", appException.getMessage());
+    }
+
+    @Test
+    public void should_verify_coordinate_inside_planet_with_y_bigger_then_planet_error() {
+        var planet = generator.nextObject(PlanetDTO.class);
+        planet.setWidth(5);
+        planet.setHeight(5);
+        Exception appException = assertThrows(Exception.class, () ->
+                planet.coordinatesIsInsidePlanet(0, 7)
+        );
+        assertEquals("Object is off planet", appException.getMessage());
+    }
+
+    @Test
+    public void should_verify_coordinate_inside_planet_with_x_and_y_bigger_then_planet_error() {
+        var planet = generator.nextObject(PlanetDTO.class);
+        planet.setWidth(5);
+        planet.setHeight(5);
+        Exception appException = assertThrows(Exception.class, () ->
+                planet.coordinatesIsInsidePlanet(7, 7)
+        );
+        assertEquals("Object is off planet", appException.getMessage());
+    }
+
+    @Test
+    public void should_verify_coordinate_inside_planet_with_x_less_then_planet_error() {
+        var planet = generator.nextObject(PlanetDTO.class);
+        planet.setWidth(5);
+        planet.setHeight(5);
+        Exception appException = assertThrows(Exception.class, () ->
+                planet.coordinatesIsInsidePlanet(-7, 0)
+        );
+        assertEquals("Object is off planet", appException.getMessage());
+    }
+
+    @Test
+    public void should_verify_coordinate_inside_planet_with_y_less_then_planet_error() {
+        var planet = generator.nextObject(PlanetDTO.class);
+        planet.setWidth(5);
+        planet.setHeight(5);
+        Exception appException = assertThrows(Exception.class, () ->
+                planet.coordinatesIsInsidePlanet(0, -7)
+        );
+        assertEquals("Object is off planet", appException.getMessage());
+    }
+
+    @Test
+    public void should_verify_coordinate_inside_planet_with_x_and_y_less_then_planet_error() {
+        var planet = generator.nextObject(PlanetDTO.class);
+        planet.setWidth(5);
+        planet.setHeight(5);
+        Exception appException = assertThrows(Exception.class, () ->
+                planet.coordinatesIsInsidePlanet(-7, -7)
+        );
+        assertEquals("Object is off planet", appException.getMessage());
+    }
+
+    @Test
+    public void should_verify_there_is_no_object_at_coordinate_with_object_in_coordinate_error() {
+        var planet = generator.nextObject(PlanetDTO.class);
+        planet.setWidth(5);
+        planet.setHeight(5);
+        planet.getObjects().get(0).setX(2);
+        planet.getObjects().get(0).setY(2);
+        Exception appException = assertThrows(Exception.class, () ->
+            planet.thereIsNoObjectAtCoordinates(2, 2)
+        );
+        assertEquals("There is already an object in this area", appException.getMessage());
+    }
+}
